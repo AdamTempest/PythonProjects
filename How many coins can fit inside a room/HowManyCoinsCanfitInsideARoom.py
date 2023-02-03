0dd48bac522c490bab5a57a92a5ddb8f0eedde8c
AdamTempest <jj030469@gmail.com>
2023-02-03 17:42:34 +0630

    Cleaned the code

--- How many coins can fit inside a room/HowManyCoinsCanfitInsideARoom.py
+++ How many coins can fit inside a room/HowManyCoinsCanfitInsideARoom.py
@@ -1,9 +1,4 @@
 from os import system
-# This progam was originally made to find out how many coins can fit inside a room.
-# Then, many thoughts surfaced one after another; 
-# What if the coin is a box? What about a sphere coin?
-# What if the room is shaped like sphere? etc etc ...
-# And those questions gave birth to this program.
 
 def main():
     system("cls")
---
+++ How many coins can fit inside a room/README.md
@@ -0,0 +1,10 @@
+# Born from Shower Thoughts
+
+This progam was originally made to find out how many coins can fit inside a room.
+
+Then, many thoughts surfaced one after another;
+What if the coin is a box? What about a sphere coin?
+
+What if the room is shaped like sphere? etc etc ...
+
+And those questions lead to creation of this program.
